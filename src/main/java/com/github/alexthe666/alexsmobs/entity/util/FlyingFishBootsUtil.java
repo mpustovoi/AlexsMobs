0d5419e4155fe08f3019cc95ef9a991e8ce9a9be
Crim <danecarmona2@gmail.com>
2024-06-16 21:49:17 +0800

    Make Flying Fish jumping consistent

--- src/main/java/com/github/alexthe666/alexsmobs/entity/util/FlyingFishBootsUtil.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/util/FlyingFishBootsUtil.java
@@ -46,7 +46,7 @@ public class FlyingFishBootsUtil {
     public static void tickFlyingFishBoots(LivingEntity fishy) {
         int boostTime = getBoostTicks(fishy);
         if(boostTime <= 15 && fishy.isInWaterOrBubble() && !fishy.onGround()){
-            if(fishy.getFluidHeight(FluidTags.WATER) < 0.2F && fishy.jumping &&( !(fishy instanceof Player) || !((Player) fishy).getAbilities().flying)){
+            if(fishy.getFluidHeight(FluidTags.WATER) < 0.4F && fishy.jumping &&( !(fishy instanceof Player) || !((Player) fishy).getAbilities().flying)){
                 final RandomSource rand = fishy.getRandom();
                 boostTime = MIN_BOOST_TIME;
                 Vec3 forward = new Vec3(0, 0.0F, 0.5F + rand.nextFloat() * 1.2F).xRot(-fishy.getXRot() * Mth.DEG_TO_RAD).yRot(-fishy.getYHeadRot() * Mth.DEG_TO_RAD);
